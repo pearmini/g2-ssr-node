bedf99292e160afa058aa73cf68403ae5fc611c5
Bairui <subairui@icloud.com>
2024-07-11 20:50:50 +0800

    Support wordcloud

--- rollup.config.js
+++ rollup.config.js
@@ -3,34 +3,13 @@ const cjs = require("@rollup/plugin-commonjs");
 const json = require("@rollup/plugin-json");
 const terser = require("@rollup/plugin-terser");
 
-const common = {
-  plugins: [resolve(), cjs(), json(), terser()],
-  external: ["@antv/g"],
-};
-
 module.exports = [
   {
-    input: "antv/g2.js",
+    input: "antv/index.js",
     output: {
-      file: "dist/g2.js",
+      file: "dist/antv.js",
       format: "cjs",
     },
-    ...common,
-  },
-  {
-    input: "antv/g.js",
-    output: {
-      file: "dist/g2.js",
-      format: "cjs",
-    },
-    ...common,
-  },
-  {
-    input: "antv/g-canvas.js",
-    output: {
-      file: "dist/g2-canvas.js",
-      format: "cjs",
-    },
-    ...common,
+    plugins: [resolve(), cjs(), json(), terser()],
   },
 ];
